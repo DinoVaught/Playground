b918aba8d0c6d806a60e046c779e304a73267157
DinoVaught <dinovaught@gmail.com>
2024-10-15 23:03:31 -0500

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgForOf} from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NgForOf],
+    styleUrls: ['./app.component.css'],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Playground';
+    title: string = ' Major Swag';
+    // categories: string[] = ["US Presidents", "Popular First Ladies", "Movie Classics", "Sports" ];
+    categories: string[] = ["US Presidents", "Popular First Ladies", "Movie Classics", "Sports", "TV Sitcoms", "Celebrity Nicknames", "Cartoon Characters", "Board Games", "Popular Snacks", "Classic TV Commercials"];
 }
